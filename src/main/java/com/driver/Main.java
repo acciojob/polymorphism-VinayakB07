b33512e6e7115b5fbc2c26f2b2bea9e4e1486ce1
Vinayak B <vinayakbalachandra07@gmail.com>
2023-01-16 10:51:30 +0530

    real ans

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,22 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-        Product p=new Product();
-        System.out.println(p.product(1,2));
-        System.out.println(p.product(1,2,3));
-        System.out.println(p.product(1.0,2.0));
-    }
-}
-class Product{
-    public int product(int x, int y) {
+    public static class Product{
+    public int product(int x,int y){
         return x*y;
     }
-    public int product(int x, int y, int z) {
+    public int product(int x,int y,int z){
         return x*y*z;
     }
-    public double product(double x, double y) {
+    public double product(double x,double y){
         return x*y;
     }
+    }
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.product(2,3);
+        p.product(2,3,4);
+        p.product(2.3,4.1);
+    }
 }
+
